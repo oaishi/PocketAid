e0c05245ae682b45e780dcebf7dd529e3286eb34
FariaOaishi <oaishi.faria@gmail.com>
2017-10-31 02:17:16 +0600

    31.09.17

--- app/src/main/java/com/example/fariahuq/pocketaid/HomePage.java
+++ app/src/main/java/com/example/fariahuq/pocketaid/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.fariahuq.pocketaid;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,17 +15,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +42,7 @@ public class HomePage extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,17 +87,23 @@ public class HomePage extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_checkup) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_first_aid) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_symptoms) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_tests) {
+
+        } else if (id == R.id.nav_reminder) {
+
+        }else if (id == R.id.nav_message) {
+
+        }else if (id == R.id.nav_hospitals) {
+
+        }else if (id == R.id.nav_fav) {
 
         }
 
@@ -98,4 +111,5 @@ public class HomePage extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
